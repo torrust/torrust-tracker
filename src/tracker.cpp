4f473fe8402d85f529b57e287bb691ae05c58e58
Naim A <naim94a@gmail.com>
2016-01-31 04:46:26 +0200

    Added copyright notice for tracker.*

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,3 +1,21 @@
+/*
+*	Copyright © 2012-2016 Naim A.
+*
+*	This file is part of UDPT.
+*
+*		UDPT is free software: you can redistribute it and/or modify
+*		it under the terms of the GNU General Public License as published by
+*		the Free Software Foundation, either version 3 of the License, or
+*		(at your option) any later version.
+*
+*		UDPT is distributed in the hope that it will be useful,
+*		but WITHOUT ANY WARRANTY; without even the implied warranty of
+*		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*		GNU General Public License for more details.
+*
+*		You should have received a copy of the GNU General Public License
+*		along with UDPT.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "tracker.hpp"
 
 namespace UDPT
