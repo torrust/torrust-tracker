d8b13e52ec2c8d6ddc1eeeadd59e7e62b520daaf
Jose Celano <josecelano@gmail.com>
2022-08-10 17:07:31 +0100

    test: added temp test for udp tracker connetion id
    I do not know yet requirements for the conenction id.
    These tests have to be changed once we find them out.

--- src/protocol/utils.rs
+++ src/protocol/utils.rs
@@ -1,8 +1,8 @@
 use std::net::SocketAddr;
 use std::time::SystemTime;
-
 use aquatic_udp_protocol::ConnectionId;
 
+/// It generates a connection id needed for the BitTorrent UDP Tracker Protocol
 pub fn get_connection_id(remote_address: &SocketAddr) -> ConnectionId {
     match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
         Ok(duration) => ConnectionId(((duration.as_secs() / 3600) | ((remote_address.port() as u64) << 36)) as i64),
@@ -33,8 +33,50 @@ pub fn ser_instant<S: serde::Serializer>(inst: &std::time::Instant, ser: S) -> R
 
 #[cfg(test)]
 mod tests {
-    use std::time::Instant;
+    use core::time;
+    use std::{time::Instant, net::{SocketAddr, IpAddr, Ipv4Addr}, thread::sleep};
+
+    #[test]
+    fn connection_id_in_udp_tracker_should_be_the_same_for_one_client_during_some_hours() {
+        let client_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+        let connection_id_1 = get_connection_id(&client_addr);
+
+        // TODO: mock time passing
+        sleep(time::Duration::from_secs(10));
+
+        let connection_id_2 = get_connection_id(&client_addr);
+
+        assert_eq!(connection_id_1, connection_id_2);
+    }
+
+    #[test]
+    fn connection_id_in_udp_tracker_should_be_different_for_each_tracker_client() {
+        let client_1_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+        let client_2_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 2)), 8080);
+
+        let connection_id_1 = get_connection_id(&client_1_addr);
+        let connection_id_2 = get_connection_id(&client_2_addr);
+
+        assert_ne!(connection_id_1, connection_id_2);
+    }
+
+    #[test]
+    fn connection_id_in_udp_tracker_should_be_different_for_the_same_tracker_client_at_different_times() {
+        let client_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+        let connection_id_1 = get_connection_id(&client_addr);
+
+        sleep(time::Duration::from_secs(2));
+
+        let connection_id_2 = get_connection_id(&client_addr);
+
+        assert_ne!(connection_id_1, connection_id_2);
+    }
+
     use serde::Serialize;
+
+    use crate::protocol::utils::get_connection_id;
 
     #[warn(unused_imports)]
     use super::ser_instant;
