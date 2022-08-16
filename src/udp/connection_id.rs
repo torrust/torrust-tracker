f2d7d5f2edb6e9e7c8c6f3076076be9ab51fa5ce
Jose Celano <josecelano@gmail.com>
2022-08-16 20:29:48 +0100

    refactor: wip. extract logic to generate time bound pepper
    Time bound pepper is a dinamycally-generated hash value which expires
    after 2 minutes. It's used to generate the UDP tracket connection IDs.

--- src/udp/connection_id.rs
+++ src/udp/connection_id.rs
@@ -1,6 +1,54 @@
+use std::ops::BitOr;
 use std::{net::SocketAddr};
 use std::net::IpAddr;
 use aquatic_udp_protocol::ConnectionId;
+use arraytools::ArrayTools;
+
+fn generate_time_bound_pepper(server_secret: &[u8; 32], current_timestamp: u64) -> [u8; 32] {
+
+    // todo: use a TimeBoundPepper struct as proposed by @da2ce7, storing also the current timestamp
+    // used to generate the TimeBoundPepper
+    //
+    // #[derive(Default)]
+    // struct TimeBoundPepper {
+    //    created: SystemTime,
+    //    pepper: [u8; 32],
+    // }
+    //
+    // impl Default for TimeBoundPepper {
+    //     fn default() -> Self { SystemTime::now(), pepper: hash(SERVER_SECRET || self.created ) };
+    // }
+    //
+    // impl TimeBoundPepper {
+    //   pub fn new() ->  Default::default();
+    //
+    //   pub fn get_pepper(&mut self, expires: std::time::Duration) -> [u8; 32] {
+    //     if (created.elapsed().unwrap() >= expires) self = self.new();
+    //     return self.pepper;
+    // }
+
+    // Time_Bound_Pepper = Hash(Static_Secret || Unix_Time_Minutes / 2)
+    // (32-bytes), cached, expires every two minutes.
+
+    let unix_time_minutes: u64 = current_timestamp / 60;
+
+    let number_of_two_minute_slots_since_unix_time: u64 = unix_time_minutes / 2;
+
+    let or_right_part: Vec<u8> = [
+        [0u8; 24].as_slice(),
+        number_of_two_minute_slots_since_unix_time.to_be_bytes().as_slice(),
+    ].concat();
+
+    //println!("OR left : {:?}", [0u8; 32]);
+    //println!("OR right: {:?}", &or_right_part);
+
+    // todo: extract function for OR operation for [u8; 32] arrays
+    let server_secret_or_two_minute_counter: [u8; 32] = server_secret.zip_with( or_right_part.try_into().unwrap(), BitOr::bitor);
+
+    let time_bound_pepper = blake3::hash(&server_secret_or_two_minute_counter);
+
+    *time_bound_pepper.as_bytes()
+}
 
 /// It generates a connection id needed for the BitTorrent UDP Tracker Protocol
 pub fn get_connection_id(server_secret: &[u8; 32], remote_address: &SocketAddr, current_timestamp: u64) -> ConnectionId {
@@ -15,6 +63,9 @@ pub fn get_connection_id(server_secret: &[u8; 32], remote_address: &SocketAddr, 
     
     ConnectionID = Hash(Time_Bound_Pepper || Authentication_String) (64-bit)
     */
+
+    // todo: not used yet. See `generate_time_bound_pepper` function above.
+    let _time_bound_pepper = generate_time_bound_pepper(server_secret, current_timestamp);
 
     let peer_ip_as_bytes = match remote_address.ip() {
         IpAddr::V4(ip) => ip.octets().to_vec(),
@@ -54,6 +105,52 @@ mod tests {
     use std::{net::{SocketAddr, IpAddr, Ipv4Addr}};
 
     const SERVER_SECRET: [u8;32] = [0;32];
+
+    #[test]
+    fn time_bound_pepper_should_be_the_same_during_each_two_minute_window_since_unix_epoch() {
+
+        // | Date                  | Timestamp | Unix Epoch in minutes | Connection IDs |
+        // |----------------------------------------------------------------------------|
+        // | 1/1/1970, 12:00:00 AM | 0         | minute 0              | X              |
+        // | 1/1/1970, 12:01:00 AM | 60        | minute 1              | X              |
+        // | 1/1/1970, 12:02:00 AM | 120       | minute 2              | Y = X          |
+        // | 1/1/1970, 12:03:00 AM | 180       | minute 3              | Y = X          |
+        // | 1/1/1970, 12:04:00 AM | 240       | minute 4              | Z != X         |
+        // | 1/1/1970, 12:05:00 AM | 300       | minute 5              | Z != X         |
+
+        let initial_timestamp = 0u64;
+
+        let time_bound_pepper = generate_time_bound_pepper(&SERVER_SECRET, initial_timestamp);
+
+        assert_eq!(time_bound_pepper, [42, 218, 131, 193, 129, 154, 83, 114, 218, 225, 35, 143, 193, 222, 209, 35, 200, 16, 79, 218, 161, 88, 98, 170, 238, 105, 66, 138, 24, 32, 252, 218]);
+
+        let time_bound_pepper_after_two_minutes = generate_time_bound_pepper(&SERVER_SECRET, initial_timestamp + 120 - 1);
+
+        assert_eq!(time_bound_pepper_after_two_minutes, time_bound_pepper);
+    }
+
+    #[test]
+    fn time_bound_pepper_should_change_after_a_two_minute_window_starting_time_windows_at_unix_epoch() {
+
+        // | Date                  | Timestamp | Unix Epoch in minutes | Connection IDs |
+        // |----------------------------------------------------------------------------|
+        // | 1/1/1970, 12:00:00 AM | 0         | minute 0              | X              |
+        // | 1/1/1970, 12:01:00 AM | 60        | minute 1              | X              |
+        // | 1/1/1970, 12:02:00 AM | 120       | minute 2              | Y = X          |
+        // | 1/1/1970, 12:03:00 AM | 180       | minute 3              | Y = X          |
+        // | 1/1/1970, 12:04:00 AM | 240       | minute 4              | Z != X         |
+        // | 1/1/1970, 12:05:00 AM | 300       | minute 5              | Z != X         |
+
+        let initial_timestamp = 0u64;
+
+        let time_bound_pepper = generate_time_bound_pepper(&SERVER_SECRET, initial_timestamp);
+
+        assert_eq!(time_bound_pepper, [42, 218, 131, 193, 129, 154, 83, 114, 218, 225, 35, 143, 193, 222, 209, 35, 200, 16, 79, 218, 161, 88, 98, 170, 238, 105, 66, 138, 24, 32, 252, 218]);
+
+        let time_bound_pepper_after_two_minutes = generate_time_bound_pepper(&SERVER_SECRET, initial_timestamp + 120 - 1);
+
+        assert_eq!(time_bound_pepper_after_two_minutes, time_bound_pepper);
+    }
 
     #[test]
     fn connection_id_is_generated_by_hashing_the_client_ip_and_port_with_a_server_secret() {
