0fee0b5e744196f5fb931835eefcfc45a2c2cd3f
Jose Celano <josecelano@gmail.com>
2022-09-05 11:22:26 +0100

    feat: new CYpher trait and Blowfish impl

---
+++ src/udp/connection/cypher.rs
@@ -0,0 +1,61 @@
+use crypto::{blowfish::Blowfish, symmetriccipher::{BlockEncryptor, BlockDecryptor}};
+
+use super::secret::Secret;
+
+trait Cypher {
+    fn encrypt(&self, decrypted_bytes: &[u8; 8]) -> [u8; 8];
+
+    fn decrypt(&self, encrypted_bytes: &[u8; 8]) -> [u8; 8];
+}
+
+struct BlowfishCypher {
+    blowfish: Blowfish
+}
+
+impl BlowfishCypher {
+    pub fn new(secret: Secret) -> Self {
+        let blowfish = Blowfish::new(&secret.to_bytes());
+        BlowfishCypher {
+            blowfish
+        }
+    }
+}
+
+impl Cypher for BlowfishCypher {
+    fn encrypt(&self, decrypted_bytes: &[u8; 8]) -> [u8; 8] {
+        let mut encrypted_bytes = [0u8; 8];
+
+        self.blowfish.encrypt_block(decrypted_bytes, &mut encrypted_bytes);
+
+        encrypted_bytes
+    }
+
+    fn decrypt(&self, encrypted_bytes: &[u8; 8]) -> [u8; 8] {
+        let mut decrypted_bytes = [0u8; 8];
+
+        self.blowfish.decrypt_block(encrypted_bytes, &mut decrypted_bytes);
+
+        decrypted_bytes
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::udp::connection::{secret::Secret, cypher::{BlowfishCypher, Cypher}};
+
+
+    #[test]
+    fn it_should_encrypt_and_decrypt_a_byte_array() {
+        let secret = Secret::new([0u8;32]);
+
+        let cypher = BlowfishCypher::new(secret);
+
+        let text = [0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8];
+
+        let encrypted_text = cypher.encrypt(&text);
+
+        let decrypted_text = cypher.decrypt(&encrypted_text);
+
+        assert_eq!(decrypted_text, text);
+    }
+}
--- src/udp/connection/mod.rs
+++ src/udp/connection/mod.rs
@@ -4,3 +4,4 @@ pub mod client_id;
 pub mod connection_id;
 pub mod timestamp_32;
 pub mod timestamp_64;
+mod cypher;
