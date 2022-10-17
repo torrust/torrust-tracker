5e2bef3ce9c2c7924de2fcc93d65debf39c784de
Cameron Garnham <me@da2ce7.com>
2022-10-17 17:03:06 +0200

    format: re-apply rust-format

--- src/tracker/statistics.rs
+++ src/tracker/statistics.rs
@@ -1,5 +1,6 @@
-use async_trait::async_trait;
 use std::sync::Arc;
+
+use async_trait::async_trait;
 use tokio::sync::mpsc::error::SendError;
 use tokio::sync::mpsc::Sender;
 use tokio::sync::{mpsc, RwLock, RwLockReadGuard};
