baba21b31f669904c2528b87d91dc07edbcaa60b
Cameron Garnham <me@da2ce7.com>
2022-11-25 20:37:27 +0100

    clippy: fix tests/udp.rs

--- tests/udp.rs
+++ tests/udp.rs
@@ -48,7 +48,7 @@ mod udp_tracker_server {
             }
         }
 
-        pub async fn start(&mut self, configuration: Arc<Configuration>) {
+        pub fn start(&mut self, configuration: &Arc<Configuration>) {
             if !self.started.load(Ordering::Relaxed) {
                 // Set the time of Torrust app starting
                 lazy_static::initialize(&static_time::TIME_AT_APP_START);
@@ -68,7 +68,7 @@ mod udp_tracker_server {
                 };
 
                 // Initialize logging
-                logging::setup(&configuration);
+                logging::setup(configuration);
 
                 let udp_tracker_config = &configuration.udp_trackers[0];
 
@@ -82,9 +82,9 @@ mod udp_tracker_server {
         }
     }
 
-    async fn new_running_udp_server(configuration: Arc<Configuration>) -> UdpServer {
+    fn new_running_udp_server(configuration: &Arc<Configuration>) -> UdpServer {
         let mut udp_server = UdpServer::new();
-        udp_server.start(configuration).await;
+        udp_server.start(configuration);
         udp_server
     }
 
@@ -101,7 +101,7 @@ mod udp_tracker_server {
         }
 
         async fn connect(&self, remote_address: &str) {
-            self.socket.connect(remote_address).await.unwrap()
+            self.socket.connect(remote_address).await.unwrap();
         }
 
         async fn send(&self, bytes: &[u8]) -> usize {
@@ -134,12 +134,13 @@ mod udp_tracker_server {
 
             let request_data = match request.write(&mut cursor) {
                 Ok(_) => {
+                    #[allow(clippy::cast_possible_truncation)]
                     let position = cursor.position() as usize;
                     let inner_request_buffer = cursor.get_ref();
                     // Return slice which contains written request data
                     &inner_request_buffer[..position]
                 }
-                Err(_) => panic!("could not write request to bytes."),
+                Err(e) => panic!("could not write request to bytes: {e}."),
             };
 
             self.udp_client.send(request_data).await
@@ -199,7 +200,7 @@ mod udp_tracker_server {
     async fn should_return_a_bad_request_response_when_the_client_sends_an_empty_request() {
         let configuration = tracker_configuration();
 
-        let udp_server = new_running_udp_server(configuration).await;
+        let udp_server = new_running_udp_server(&configuration);
 
         let client = new_connected_udp_client(&udp_server.bind_address.unwrap()).await;
 
@@ -216,7 +217,7 @@ mod udp_tracker_server {
     async fn should_return_a_connect_response_when_the_client_sends_a_connection_request() {
         let configuration = tracker_configuration();
 
-        let udp_server = new_running_udp_server(configuration).await;
+        let udp_server = new_running_udp_server(&configuration);
 
         let client = new_connected_udp_tracker_client(&udp_server.bind_address.unwrap()).await;
 
@@ -248,7 +249,7 @@ mod udp_tracker_server {
     async fn should_return_an_announce_response_when_the_client_sends_an_announce_request() {
         let configuration = tracker_configuration();
 
-        let udp_server = new_running_udp_server(configuration).await;
+        let udp_server = new_running_udp_server(&configuration);
 
         let client = new_connected_udp_tracker_client(&udp_server.bind_address.unwrap()).await;
 
@@ -282,7 +283,7 @@ mod udp_tracker_server {
     async fn should_return_a_scrape_response_when_the_client_sends_a_scrape_request() {
         let configuration = tracker_configuration();
 
-        let udp_server = new_running_udp_server(configuration).await;
+        let udp_server = new_running_udp_server(&configuration);
 
         let client = new_connected_udp_tracker_client(&udp_server.bind_address.unwrap()).await;
 
