c57d4fcb90fae3bb5f83c2182d6f82b56b744957
Jose Celano <josecelano@gmail.com>
2022-12-10 13:53:11 +0000

    feat: use long option names for docker commands

--- docker/bin/run.sh
+++ docker/bin/run.sh
@@ -4,6 +4,7 @@ TORRUST_TRACKER_USER_UID=${TORRUST_TRACKER_USER_UID:-1000}
 
 docker run -it \
     --user="$TORRUST_TRACKER_USER_UID" \
-    -p 6969:6969/udp -p 1212:1212 \
+    --publish 6969:6969/udp \
+    --publish 1212:1212 \
     --volume "$(pwd)/storage":"/app/storage" \
     torrust-tracker
