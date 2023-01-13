1c72ac078bca8e5750d35f82184504ee891304d5
Jose Celano <josecelano@gmail.com>
2023-01-13 18:39:02 +0000

    docs(api): [#143] remove deprecated comment

--- src/apis/resources/mod.rs
+++ src/apis/resources/mod.rs
@@ -1,12 +1,3 @@
-//! These are the Rest API resources.
-//!
-//! WIP. Not all endpoints have their resource structs.
-//!
-//! - [x] `AuthKeys`
-//! - [ ] `Torrent`, `ListItem`, `Peer`, `PeerId`
-//! - [ ] `StatsResource`
-//! - [ ] ...
-
 pub mod auth_key;
 pub mod peer;
 pub mod stats;
