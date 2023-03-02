ee5b0886ec510b1c775a7664c58ca8070c2e8074
Jose Celano <josecelano@gmail.com>
2023-03-02 11:49:40 +0000

    docs(http): add comment for duplicate struct info

--- src/tracker/torrent.rs
+++ src/tracker/torrent.rs
@@ -14,6 +14,8 @@ pub struct Entry {
     pub completed: u32,
 }
 
+/// Swarm metadata dictionary in the scrape response.
+/// BEP 48: <https://www.bittorrent.org/beps/bep_0048.html>
 #[derive(Debug, PartialEq, Default)]
 pub struct SwarmMetadata {
     pub complete: u32,   // The number of active peers that have completed downloading (seeders)
@@ -26,6 +28,14 @@ impl SwarmMetadata {
     pub fn zeroed() -> Self {
         Self::default()
     }
+}
+
+/// Swarm statistics. Alternative struct for swarm metadata in scrape response.
+#[derive(Debug)]
+pub struct SwamStats {
+    pub completed: u32, // The number of peers that have ever completed downloading
+    pub seeders: u32,   // The number of active peers that have completed downloading (seeders)
+    pub leechers: u32,  // The number of active peers that have not completed downloading (leechers)
 }
 
 impl Entry {
@@ -109,13 +119,6 @@ impl Default for Entry {
     fn default() -> Self {
         Self::new()
     }
-}
-
-#[derive(Debug)]
-pub struct SwamStats {
-    pub completed: u32,
-    pub seeders: u32,
-    pub leechers: u32,
 }
 
 #[cfg(test)]
