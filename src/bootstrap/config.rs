0adf373bca011c7985277066a7f20138785bc35f
Jose Celano <josecelano@gmail.com>
2023-08-08 10:38:43 +0100

    refactor: [#361] rename constants representing env vars
    All of then have the ENV_VAR prefix now. Like in the Index Backend.
    So we can identify constants that are related to env vars.

--- src/bootstrap/config.rs
+++ src/bootstrap/config.rs
@@ -7,14 +7,19 @@ use torrust_tracker_configuration::Configuration;
 
 // Environment variables
 
-const CONFIG_PATH: &str = "./config.toml";
-const CONFIG_ENV_VAR_NAME: &str = "TORRUST_TRACKER_CONFIG";
+/// The whole `config.toml` file content. It has priority over the config file.
+/// Even if the file is not on the default path.
+const ENV_VAR_CONFIG: &str = "TORRUST_TRACKER_CONFIG";
+
+// Default values
+
+const ENV_VAR_DEFAULT_CONFIG_PATH: &str = "./config.toml";
 
 /// It loads the application configuration from the environment.
 ///
 /// There are two methods to inject the configuration:
 ///
-/// 1. By using a config file: `config.toml`. The file must be in the same folder where you are running the tracker.
+/// 1. By using a config file: `config.toml`.
 /// 2. Environment variable: `TORRUST_TRACKER_CONFIG`. The variable contains the same contents as the `config.toml` file.
 ///
 /// Environment variable has priority over the config file.
@@ -27,11 +32,13 @@ const CONFIG_ENV_VAR_NAME: &str = "TORRUST_TRACKER_CONFIG";
 /// `./config.toml` file or the env var `TORRUST_TRACKER_CONFIG`.
 #[must_use]
 pub fn initialize_configuration() -> Configuration {
-    if env::var(CONFIG_ENV_VAR_NAME).is_ok() {
-        println!("Loading configuration from env var {CONFIG_ENV_VAR_NAME}");
-        Configuration::load_from_env_var(CONFIG_ENV_VAR_NAME).unwrap()
+    if env::var(ENV_VAR_CONFIG).is_ok() {
+        println!("Loading configuration from env var {ENV_VAR_CONFIG}");
+
+        Configuration::load_from_env_var(ENV_VAR_CONFIG).unwrap()
     } else {
-        println!("Loading configuration from config file {CONFIG_PATH}");
-        Configuration::load_from_file(CONFIG_PATH).unwrap()
+        println!("Loading configuration from config file {ENV_VAR_DEFAULT_CONFIG_PATH}");
+
+        Configuration::load_from_file(ENV_VAR_DEFAULT_CONFIG_PATH).unwrap()
     }
 }
