1475eadd25e7fbf208523f72f49188d3f28173c2
Jose Celano <josecelano@gmail.com>
2024-05-10 11:12:53 +0100

    refactor: [#852] eenrich field types in UdpTracker config struct

--- packages/configuration/src/v1/udp_tracker.rs
+++ packages/configuration/src/v1/udp_tracker.rs
@@ -1,3 +1,5 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
@@ -8,13 +10,13 @@ pub struct UdpTracker {
     /// The format is `ip:port`, for example `0.0.0.0:6969`. If you want to
     /// listen to all interfaces, use `0.0.0.0`. If you want the operating
     /// system to choose a random port, use port `0`.
-    pub bind_address: String,
+    pub bind_address: SocketAddr,
 }
 impl Default for UdpTracker {
     fn default() -> Self {
         Self {
             enabled: false,
-            bind_address: String::from("0.0.0.0:6969"),
+            bind_address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 6969),
         }
     }
 }
