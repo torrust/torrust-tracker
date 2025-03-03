aaf74446c12e789a53d72627709c263407510750
Jose Celano <josecelano@gmail.com>
2025-03-03 18:18:12 +0000

    chore: [#1345] add git hook scripts

---
+++ contrib/dev-tools/git/hooks/pre-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cargo +nightly fmt --check &&
+    cargo +nightly check --tests --benches --examples --workspace --all-targets --all-features &&
+    cargo +nightly doc --no-deps --bins --examples --workspace --all-features &&
+    cargo +nightly machete &&
+    cargo +stable build &&
+    CARGO_INCREMENTAL=0 cargo +stable clippy --no-deps --tests --benches --examples --workspace --all-targets --all-features -- -D clippy::correctness -D clippy::suspicious -D clippy::complexity -D clippy::perf -D clippy::style -D clippy::pedantic &&
+    cargo +stable test --tests --benches --examples --workspace --all-targets --all-features &&
+    cargo +stable run --bin e2e_tests_runner -- --config-toml-path "./share/default/config/tracker.e2e.container.sqlite3.toml"
