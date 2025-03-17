1f30f8ef3d734051bc5f79d76200f0a5c786a737
Jose Celano <josecelano@gmail.com>
2025-03-17 12:00:03 +0000

    ci: update git hooks scripts
    To also run doctests.

--- contrib/dev-tools/git/hooks/pre-commit.sh
+++ contrib/dev-tools/git/hooks/pre-commit.sh
@@ -6,4 +6,5 @@ cargo +nightly fmt --check &&
     cargo +nightly machete &&
     cargo +stable build &&
     CARGO_INCREMENTAL=0 cargo +stable clippy --no-deps --tests --benches --examples --workspace --all-targets --all-features -- -D clippy::correctness -D clippy::suspicious -D clippy::complexity -D clippy::perf -D clippy::style -D clippy::pedantic &&
+    cargo +stable test --doc --workspace &&
     cargo +stable test --tests --benches --examples --workspace --all-targets --all-features
