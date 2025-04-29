d5e7c36ad032de32ccc44d5b81ac2a0bac9cc24b
Jose Celano <josecelano@gmail.com>
2025-04-29 12:18:58 +0100

    feat: [#1477] improve shutting down message for http servers

--- packages/axum-server/src/signals.rs
+++ packages/axum-server/src/signals.rs
@@ -8,14 +8,20 @@ use tracing::instrument;
 pub async fn graceful_shutdown(handle: axum_server::Handle, rx_halt: tokio::sync::oneshot::Receiver<Halted>, message: String) {
     shutdown_signal_with_message(rx_halt, message).await;
 
-    tracing::debug!("Sending graceful shutdown signal");
+    let duration = Duration::from_secs(90);
+
+    tracing::info!(
+        "Http server received shutdown signal, shutting down server listening on: {:?}",
+        handle.listening().await
+    );
+
     handle.graceful_shutdown(Some(Duration::from_secs(90)));
 
-    println!("!! shuting down in 90 seconds !!");
+    tracing::info!("!! Shuting down in {} seconds !!", duration.as_secs());
 
     loop {
         sleep(Duration::from_secs(1)).await;
 
-        tracing::info!("remaining alive connections: {}", handle.connection_count());
+        tracing::info!("Remaining alive connections: {}", handle.connection_count());
     }
 }
