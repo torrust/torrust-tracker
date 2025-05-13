1eb545c0fb233bba0206c2557401c2b4c686cc3a
Jose Celano <josecelano@gmail.com>
2025-05-13 09:16:15 +0100

    feat: [#1358] remove persistent metric from torrent-repository pkg
    This package dones not have persistence. Persistence is only handle in
    the `tracker-core` pacakge. The metric will be included there.

--- packages/torrent-repository/src/statistics/mod.rs
+++ packages/torrent-repository/src/statistics/mod.rs
@@ -8,7 +8,6 @@ use torrust_tracker_metrics::metric_name;
 use torrust_tracker_metrics::unit::Unit;
 
 const TORRENT_REPOSITORY_RUNTIME_TORRENTS_DOWNLOADS_TOTAL: &str = "torrent_repository_runtime_torrents_downloads_total";
-const TORRENT_REPOSITORY_PERSISTENT_TORRENTS_DOWNLOADS_TOTAL: &str = "torrent_repository_persistent_torrents_downloads_total";
 
 #[must_use]
 pub fn describe_metrics() -> Metrics {
@@ -19,14 +18,6 @@ pub fn describe_metrics() -> Metrics {
         Some(Unit::Count),
         Some(&MetricDescription::new(
             "The total number of torrent downloads since the tracker process started.",
-        )),
-    );
-
-    metrics.metric_collection.describe_counter(
-        &metric_name!(TORRENT_REPOSITORY_PERSISTENT_TORRENTS_DOWNLOADS_TOTAL),
-        Some(Unit::Count),
-        Some(&MetricDescription::new(
-            "The total number of torrent downloads since persistent statistics were enabled the first time.",
         )),
     );
 
