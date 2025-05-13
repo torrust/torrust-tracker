269d27398975df921a846770e58b4d0a5bfde256
Jose Celano <josecelano@gmail.com>
2025-05-13 20:14:31 +0100

    refactor: [#1358] rename metric
    From `TORRENT_REPOSITORY_RUNTIME_TORRENTS_DOWNLOADS_TOTAL` to `TORRENT_REPOSITORY_TORRENTS_DOWNLOADS_TOTAL`.
    
    None of the metrics in the `torrent-repositry` package will be
    persisted. We can use the `persitent` sufix for metrics in other
    packages to avoid conflicts.
    
    It's planned to use the same metric in the `tracker-core` package but
    with the historial persited value.

--- packages/torrent-repository/src/statistics/mod.rs
+++ packages/torrent-repository/src/statistics/mod.rs
@@ -8,7 +8,7 @@ use torrust_tracker_metrics::metric_name;
 use torrust_tracker_metrics::unit::Unit;
 
 const TORRENT_REPOSITORY_TORRENTS_TOTAL: &str = "torrent_repository_torrents_total";
-const TORRENT_REPOSITORY_RUNTIME_TORRENTS_DOWNLOADS_TOTAL: &str = "torrent_repository_runtime_torrents_downloads_total";
+const TORRENT_REPOSITORY_TORRENTS_DOWNLOADS_TOTAL: &str = "torrent_repository_torrents_downloads_total";
 
 #[must_use]
 pub fn describe_metrics() -> Metrics {
@@ -21,7 +21,7 @@ pub fn describe_metrics() -> Metrics {
     );
 
     metrics.metric_collection.describe_counter(
-        &metric_name!(TORRENT_REPOSITORY_RUNTIME_TORRENTS_DOWNLOADS_TOTAL),
+        &metric_name!(TORRENT_REPOSITORY_TORRENTS_DOWNLOADS_TOTAL),
         Some(Unit::Count),
         Some(&MetricDescription::new(
             "The total number of torrent downloads since the tracker process started.",
