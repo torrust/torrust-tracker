a5524825452e82a37c25462fd101d6d2023a36bb
Jose Celano <josecelano@gmail.com>
2025-06-16 17:28:22 +0100

    refactor: [#1581] finished. Global metrics in API loaded from labeled metrics

--- packages/axum-rest-tracker-api-server/src/v1/context/stats/handlers.rs
+++ packages/axum-rest-tracker-api-server/src/v1/context/stats/handlers.rs
@@ -41,21 +41,13 @@ pub struct QueryParams {
 pub async fn get_stats_handler(
     State(state): State<(
         Arc<InMemoryTorrentRepository>,
-        Arc<RwLock<BanService>>,
         Arc<bittorrent_tracker_core::statistics::repository::Repository>,
         Arc<bittorrent_http_tracker_core::statistics::repository::Repository>,
         Arc<torrust_udp_tracker_server::statistics::repository::Repository>,
     )>,
     params: Query<QueryParams>,
 ) -> Response {
-    let metrics = get_metrics(
-        state.0.clone(),
-        state.1.clone(),
-        state.2.clone(),
-        state.3.clone(),
-        state.4.clone(),
-    )
-    .await;
+    let metrics = get_metrics(state.0.clone(), state.1.clone(), state.2.clone(), state.3.clone()).await;
 
     match params.0.format {
         Some(format) => match format {
--- packages/rest-tracker-api-core/src/statistics/services.rs
+++ packages/rest-tracker-api-core/src/statistics/services.rs
@@ -27,39 +27,13 @@ pub struct TrackerMetrics {
 /// It returns all the [`TrackerMetrics`]
 pub async fn get_metrics(
     in_memory_torrent_repository: Arc<InMemoryTorrentRepository>,
-    ban_service: Arc<RwLock<BanService>>,
     tracker_core_stats_repository: Arc<bittorrent_tracker_core::statistics::repository::Repository>,
     http_stats_repository: Arc<bittorrent_http_tracker_core::statistics::repository::Repository>,
     udp_server_stats_repository: Arc<udp_server_statistics::repository::Repository>,
 ) -> TrackerMetrics {
-    let protocol_metrics_from_global_metrics = get_protocol_metrics(
-        ban_service.clone(),
-        http_stats_repository.clone(),
-        udp_server_stats_repository.clone(),
-    )
-    .await;
-
-    let protocol_metrics_from_labeled_metrics =
-        get_protocol_metrics_from_labeled_metrics(http_stats_repository.clone(), udp_server_stats_repository.clone()).await;
-
-    // todo:
-    // We keep both metrics until we deploy to production and we can
-    // ensure that the protocol metrics from labeled metrics are correct.
-    // After that we can remove the `get_protocol_metrics` function and
-    // use only the `get_protocol_metrics_from_labeled_metrics` function.
-    // And also remove the code in repositories to generate the global metrics.
-    let protocol_metrics = if protocol_metrics_from_global_metrics == protocol_metrics_from_labeled_metrics {
-        protocol_metrics_from_labeled_metrics
-    } else {
-        tracing::warn!("The protocol metrics from global metrics and labeled metrics are different");
-        tracing::warn!("Global metrics: {:?}", protocol_metrics_from_global_metrics);
-        tracing::warn!("Labeled metrics: {:?}", protocol_metrics_from_labeled_metrics);
-        protocol_metrics_from_global_metrics
-    };
-
     TrackerMetrics {
         torrents_metrics: get_torrents_metrics(in_memory_torrent_repository, tracker_core_stats_repository).await,
-        protocol_metrics,
+        protocol_metrics: get_protocol_metrics(http_stats_repository.clone(), udp_server_stats_repository.clone()).await,
     }
 }
 
@@ -77,56 +51,8 @@ async fn get_torrents_metrics(
 }
 
 #[allow(deprecated)]
-async fn get_protocol_metrics(
-    ban_service: Arc<RwLock<BanService>>,
-    http_stats_repository: Arc<bittorrent_http_tracker_core::statistics::repository::Repository>,
-    udp_server_stats_repository: Arc<udp_server_statistics::repository::Repository>,
-) -> ProtocolMetrics {
-    let udp_banned_ips_total = ban_service.read().await.get_banned_ips_total();
-    let http_stats = http_stats_repository.get_stats().await;
-    let udp_server_stats = udp_server_stats_repository.get_stats().await;
-
-    // For backward compatibility we keep the `tcp4_connections_handled` and
-    // `tcp6_connections_handled` metrics. They don't make sense for the HTTP
-    // tracker, but we keep them for now. In new major versions we should remove
-    // them.
-
-    ProtocolMetrics {
-        // TCPv4
-        tcp4_connections_handled: http_stats.tcp4_announces_handled() + http_stats.tcp4_scrapes_handled(),
-        tcp4_announces_handled: http_stats.tcp4_announces_handled(),
-        tcp4_scrapes_handled: http_stats.tcp4_scrapes_handled(),
-        // TCPv6
-        tcp6_connections_handled: http_stats.tcp6_announces_handled() + http_stats.tcp6_scrapes_handled(),
-        tcp6_announces_handled: http_stats.tcp6_announces_handled(),
-        tcp6_scrapes_handled: http_stats.tcp6_scrapes_handled(),
-        // UDP
-        udp_requests_aborted: udp_server_stats.udp_requests_aborted(),
-        udp_requests_banned: udp_server_stats.udp_requests_banned(),
-        udp_banned_ips_total: udp_banned_ips_total as u64,
-        udp_avg_connect_processing_time_ns: udp_server_stats.udp_avg_connect_processing_time_ns(),
-        udp_avg_announce_processing_time_ns: udp_server_stats.udp_avg_announce_processing_time_ns(),
-        udp_avg_scrape_processing_time_ns: udp_server_stats.udp_avg_scrape_processing_time_ns(),
-        // UDPv4
-        udp4_requests: udp_server_stats.udp4_requests(),
-        udp4_connections_handled: udp_server_stats.udp4_connections_handled(),
-        udp4_announces_handled: udp_server_stats.udp4_announces_handled(),
-        udp4_scrapes_handled: udp_server_stats.udp4_scrapes_handled(),
-        udp4_responses: udp_server_stats.udp4_responses(),
-        udp4_errors_handled: udp_server_stats.udp4_errors_handled(),
-        // UDPv6
-        udp6_requests: udp_server_stats.udp6_requests(),
-        udp6_connections_handled: udp_server_stats.udp6_connections_handled(),
-        udp6_announces_handled: udp_server_stats.udp6_announces_handled(),
-        udp6_scrapes_handled: udp_server_stats.udp6_scrapes_handled(),
-        udp6_responses: udp_server_stats.udp6_responses(),
-        udp6_errors_handled: udp_server_stats.udp6_errors_handled(),
-    }
-}
-
-#[allow(deprecated)]
 #[allow(clippy::too_many_lines)]
-async fn get_protocol_metrics_from_labeled_metrics(
+async fn get_protocol_metrics(
     http_stats_repository: Arc<bittorrent_http_tracker_core::statistics::repository::Repository>,
     udp_server_stats_repository: Arc<udp_server_statistics::repository::Repository>,
 ) -> ProtocolMetrics {
@@ -307,7 +233,7 @@ mod tests {
         let tracker_core_container =
             TrackerCoreContainer::initialize_from(&core_config, &swarm_coordination_registry_container.clone());
 
-        let ban_service = Arc::new(RwLock::new(BanService::new(MAX_CONNECTION_ID_ERRORS_PER_IP)));
+        let _ban_service = Arc::new(RwLock::new(BanService::new(MAX_CONNECTION_ID_ERRORS_PER_IP)));
 
         // HTTP core stats
         let http_core_broadcaster = Broadcaster::default();
@@ -326,7 +252,6 @@ mod tests {
 
         let tracker_metrics = get_metrics(
             tracker_core_container.in_memory_torrent_repository.clone(),
-            ban_service.clone(),
             tracker_core_container.stats_repository.clone(),
             http_stats_repository.clone(),
             udp_server_stats_repository.clone(),
