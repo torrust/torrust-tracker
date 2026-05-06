3068b9e14f4531f7d9bafbb3aac5f0b84e8b5f9f
Jose Celano <josecelano@gmail.com>
2026-05-06 13:50:07 +0100

    refactor(peer-id): split crate into focused internal modules

--- packages/peer-id/src/lib.rs
+++ packages/peer-id/src/lib.rs
@@ -2,6 +2,8 @@
 
 #![allow(clippy::module_name_repetitions)]
 
+mod peer_client;
 mod peer_id;
 
-pub use self::peer_id::{PeerClient, PeerId};
+pub use self::peer_client::PeerClient;
+pub use self::peer_id::PeerId;
